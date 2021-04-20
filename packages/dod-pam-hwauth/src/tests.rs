b4d674d24636cc240eb48a5be9a9fefe59d69e2e
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2021-04-19 20:04:21 -0400

    removed tarpaulin

--- packages/dod-pam-hwauth/src/tests.rs
+++ packages/dod-pam-hwauth/src/tests.rs
@@ -4,21 +4,8 @@
 
 use super::*;
 
-use cargo_tarpaulin::config::Config;
-use cargo_tarpaulin::launch_tarpaulin;
-//use cargo_tarpaulin::traces::CoverageStat;
-
 #[test]
 fn test_auth_withbad_data() {
-
-  let mut config = Config::default();
-  config.verbose = true;
-  config.test_timeout = std::time::Duration::from_secs(3);
-  config.manifest = std::env::current_dir();
-  config.manifest.push("Cargo.toml");
-
-  let (res, ret) = launch_tarpaulin(&config, &None).unwrap();
-
   unsafe {
     let r = pam_sm_setcred(
       std::mem::transmute(0 as u8),
